2d30217fdbd1217a791255006e5df72ba7c0a1df
Alexey Pogorelov <alexpogo471@gmail.com>
2020-07-08 07:51:26 +0300

    basic tests implemented

--- src/test/java/CalcTest.java
+++ src/test/java/CalcTest.java
@@ -1,6 +1,27 @@
-
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class CalcTest {
 
+
+    @Test
+    public void testSum(Number result,Number arg1, Number arg2){
+        Assert.assertEquals(result,Calc.sum(arg1,arg2),"Неверный результат");
+    }
+
+    @Test
+    public void testMinus(Number result,Number arg1, Number arg2){
+        Assert.assertEquals(result,Calc.minus(arg1,arg2),"Неверный результат");
+    }
+
+    @Test
+    public void testMulti(Number result,Number arg1, Number arg2){
+        Assert.assertEquals(result,Calc.multi(arg1,arg2),"Неверный результат");
+    }
+
+    @Test
+    public void testDiv(Number result,Number arg1, Number arg2){
+        Assert.assertEquals(result,Calc.div(arg1,arg2),"Неверный результат");
+    }
 
 }
