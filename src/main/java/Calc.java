c438ccd653ddde9e4bacd34622c762e18871c781
Alexey Pogorelov <alexpogo471@gmail.com>
2020-07-08 09:03:41 +0300

    add dataProviders for methods

--- src/main/java/Calc.java
+++ src/main/java/Calc.java
@@ -1,17 +1,45 @@
 public class Calc {
     public static Number sum(Number arg1, Number arg2) {
-        return 5;
+        if (arg1 instanceof Integer && arg2 instanceof Integer) {
+            return arg1.intValue() + arg2.intValue();
+        } else if (arg1 instanceof Double || arg2 instanceof Double) {
+            return arg1.doubleValue() + arg2.doubleValue();
+
+        }
+        return null;
     }
 
     public static Number minus(Number arg1, Number arg2) {
-        return 5;
+        if (arg1 instanceof Integer && arg2 instanceof Integer) {
+            return arg1.intValue() - arg2.intValue();
+        } else if (arg1 instanceof Double || arg2 instanceof Double) {
+            return arg1.doubleValue() - arg2.doubleValue();
+
+        }
+        return null;
     }
 
     public static Number multi(Number arg1, Number arg2) {
-        return 5;
+        if (arg1 instanceof Integer && arg2 instanceof Integer) {
+            return arg1.intValue() * arg2.intValue();
+        } else if (arg1 instanceof Double || arg2 instanceof Double) {
+            return arg1.doubleValue() * arg2.doubleValue();
+
+        }
+        return null;
     }
 
     public static Number div(Number arg1, Number arg2) {
-        return 5;
+        if (arg1 instanceof Integer && arg2 instanceof Integer) {
+            return arg1.intValue() / arg2.intValue();
+        } else if (arg1 instanceof Double || arg2 instanceof Double) {
+            return arg1.doubleValue() / arg2.doubleValue();
+
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(3/2);
     }
 }
--- src/test/java/CalcTest.java
+++ src/test/java/CalcTest.java
@@ -6,28 +6,57 @@ public class CalcTest {
 
 
     @DataProvider
-    public Object[][] numberProvider(){
+    public Object[][] numberProviderForSum(){
         return new Object[][]{
-                {5,3,2}
+                {5,3,2},
+                {3.27,3,0.27},
+                {-8.769,-3.040,-5.729}
         };
     }
 
-    @Test(dataProvider = "numberProvider")
+    @DataProvider
+    public Object[][] numberProviderForMinus(){
+        return new Object[][]{
+                {1,3,2},
+                {2.73,3,0.27},
+                {8.769,3.040,-5.729}
+        };
+    }
+
+    @DataProvider
+    public Object[][] numberProviderForMulti(){
+        return new Object[][]{
+                {6,3,2},
+                {0.81,3,0.27},
+                {17.41616,-3.040,-5.729}
+        };
+    }
+
+    @DataProvider
+    public Object[][] numberProviderForDiv(){
+        return new Object[][]{
+                {1.5,3.0,2.0},
+                {6.0,3,0.5},
+                {6.0,-3.6,-0.6}
+        };
+    }
+
+    @Test(dataProvider = "numberProviderForSum")
     public void testSum(Number result,Number arg1, Number arg2){
         Assert.assertEquals(Calc.sum(arg1,arg2),result,"Неверный результат");
     }
 
-    @Test(dataProvider = "numberProvider")
+    @Test(dataProvider = "numberProviderForMinus")
     public void testMinus(Number result,Number arg1, Number arg2){
         Assert.assertEquals(Calc.minus(arg1,arg2),result,"Неверный результат");
     }
 
-    @Test(dataProvider = "numberProvider")
+    @Test(dataProvider = "numberProviderForMulti")
     public void testMulti(Number result,Number arg1, Number arg2){
         Assert.assertEquals(Calc.multi(arg1,arg2),result,"Неверный результат");
     }
 
-    @Test(dataProvider = "numberProvider")
+    @Test(dataProvider = "numberProviderForDiv")
     public void testDiv(Number result,Number arg1, Number arg2){
         Assert.assertEquals(Calc.div(arg1,arg2),result,"Неверный результат");
     }
